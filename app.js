d7743de6d6b1ec844172a1d4dceef76576e11470
Siva-Sankar19998 <kalyanmuralasetti@gmail.com>
2024-03-09 21:38:33 +0530

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 
 const Head=()=>{
-    console.log('checking code');
-    console.warn('warning code');
     const heading =React.createElement('div',{id:"parent"},'I am Main Tag');
     return heading;
 }
-
-
-// console.log(heading,'heading');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(Head);
